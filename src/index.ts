5a823b7e8a1b58918ea0a99a58816a0021d16848
danzelito <82509270+danzelito@users.noreply.github.com>
2022-11-01 06:55:20 +0200

    Initializing and first steps

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuid } from 'uuid';
 
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>('#list');
+const form = document.querySelector<HTMLFormElement>('#task-form');
+const input = document.querySelector<HTMLInputElement>('task-title');
